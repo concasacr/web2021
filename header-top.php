46116209e109133912e03f63e435bc85a2ee5dfa
concasacr <85511425+concasacr@users.noreply.github.com>
2021-06-08 14:47:41 -0600

    Update header-top.php

--- header-top.php
+++ header-top.php
@@ -30,10 +30,10 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
     <!-- CSS Styles -->
-    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/n-css/style-canvas.css">
-    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/n-css/all.css">
-    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/n-css/animate.css" type="text/css" />
-    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/n-css/responsive.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/css/n-css/style-canvas.css">
+    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/css/n-css/all.css">
+    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/css/n-css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.concasa.com/concasa.com/css/n-css/responsive.css" type="text/css" />
     <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
 
     <!-- Icons -->
