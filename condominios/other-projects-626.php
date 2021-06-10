0de67bfa218f1a50ebe352d608d903b2a9032408
concasacr <85511425+concasacr@users.noreply.github.com>
2021-06-10 08:43:26 -0600

    Add files via upload

---
+++ condominios/other-projects-626.php
@@ -0,0 +1,95 @@
+<section id="section-projects" class="page-section bottom60">
+   <div class="container-fullwidth clearfix">
+      <div class="heading-block center">
+         <h2>Otros proyectos <span>disponibles</span></h2>
+         <span>Preventa, entrega inmediata y a plazo</span>
+      </div>
+      <div id="oc-portfolio" class="owl-carousel portfolio-carousel carousel-widget" data-margin="20" data-loop="true" data-nav="true" data-pagi="false" data-items-xs="1" data-items-sm="2" data-items-md="3" data-items-lg="4">
+         <div class="oc-item">
+            <div class="iportfolio">
+               <div class="portfolio-image">
+                  <a href="./condominio-terrazas-del-norte">
+                  <img src="../n-images/proyectos/general/proyectos-tn.jpg" alt="Condomino - Terrazas del Norte">
+                  </a>
+                  <div class="portfolio-overlay">
+                     <a href="./condominio-terrazas-del-norte" class="right-icon">$<?=number_format(getPrecioProperty('Terrazas','2'), 0, '.', ',');?> USD<br><span> 3 Habitaciones</span></a>
+                  </div>
+               </div>
+               <div class="portfolio-desc">
+                  <h3 class="txtconcasaini"><a href="./condominio-terrazas-del-norte">Terrazas del Norte</a></h3>
+                  <span><a href="./condominio-terrazas-del-norte"><i class="icon-map-pin"></i> San Rafael, Alajuela</a></span>
+               </div>
+            </div>
+         </div>
+         <div class="oc-item">
+            <div class="iportfolio">
+               <div class="portfolio-image">
+                  <a href="./condominio-costa-linda">
+                  <img src="../n-images/proyectos/general/proyectos-cl.jpg" alt="Condomino - Costa Linda">
+                  </a>
+                  <div class="portfolio-overlay">
+                     <a href="./condominio-costa-linda" class="right-icon">$<?=number_format(getPrecioProperty('Costa','1'), 0, '.', ',');?> USD<br><span>2 Habitaciones</span></a>
+                  </div>
+               </div>
+               <div class="portfolio-desc">
+                  <h3 class="txtconcasaini"><a href="./condominio-costa-linda">Costa Linda</a></h3>
+                  <span><a href="./condominio-costa-linda"><i class="icon-map-pin"></i> Jac&oacute;</a></span>
+               </div>
+            </div>
+         </div>
+         <div class="oc-item">
+            <div class="iportfolio">
+               <div class="portfolio-image">
+                  <a href="./condominio-monte-alto">
+                  <img src="../n-images/proyectos/general/proyectos-ma.jpg" alt="Condomino - >Monte Alto">
+                  </a>
+                  <div class="portfolio-overlay">
+                     <a href="./condominio-monte-alto" class="right-icon">$<?=number_format(getPrecioProperty('Monte','1'), 0, '.', ',');?> USD<br><span>2 y 3 Habitaciones</span></a>
+                  </div>
+               </div>
+               <div class="portfolio-desc">
+                  <h3 class="txtconcasaini"><a href="./condominio-monte-alto">Monte Alto</a></h3>
+                  <span><a href="./condominio-monte-alto"><i class="icon-map-pin"></i> Curridabat</a></span>
+               </div>
+            </div>
+         </div>
+         <div class="oc-item">
+            <div class="iportfolio">
+               <div class="portfolio-image">
+                  <a href="./condominio-caminos-del-bosque">
+                  <img src="../n-images/proyectos/general/proyectos-cb.jpg" alt="Condomino - Caminos del Bosque">
+                  </a>
+                  <div class="portfolio-overlay">
+                     <a href="./condominio-caminos-del-bosque" class="right-icon">$<?=number_format(getPrecioProperty('Bosque','1'), 0, '.', ',');?> USD<br><span>2 Habitaciones</span></a>
+                  </div>
+               </div>
+               <div class="portfolio-desc">
+                  <h3 class="txtconcasaini"><a href="./condominio-caminos-del-bosque">Caminos del Bosque</a></h3>
+                  <span><a href="./condominio-caminos-del-bosque"><i class="icon-map-pin"></i> San Rafael, Alajuela</a></span>
+               </div>
+            </div>
+         </div>
+         
+         
+         <div class="oc-item">
+            <div class="iportfolio">
+               <div class="portfolio-image">
+                  <a href="./condominio-caoba">
+                  <img src="../n-images/proyectos/general/proyectos-ccb.jpg" alt="Condomino - Caoba">
+                  </a>
+                  <div class="portfolio-overlay">
+                     <a href="./condominio-caoba" class="right-icon">$<?=number_format(getPrecioProperty('Caoba','1'), 0, '.', ',');?> USD<br><span>3 Habitaciones</span></a>
+                  </div>
+               </div>
+               <div class="portfolio-desc">
+                  <h3 class="txtconcasaini"><a href="./condominio-caoba">Caoba</a></h3>
+                  <span><a href="./condominio-caoba"><i class="icon-map-pin"></i> Ciudad Colón</a></span>
+               </div>
+            </div>
+         </div>
+         
+         
+         
+      </div>
+   </div>
+</section>
