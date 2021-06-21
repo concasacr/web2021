14506d41f2ea27e85b6317401f4a284c3219dbd1
concasacr <85511425+concasacr@users.noreply.github.com>
2021-06-21 10:48:43 -0600

    Update servicio-post-venta.php

--- servicio-post-venta.php
+++ servicio-post-venta.php
@@ -63,7 +63,7 @@ div#whating strong {
 	
 
 
-<section id="page-title" class="page-title-parallax page-title-dark" style="padding: 250px 0; background-color: #ff5733; background-image: url('//cdn.concasa.com/concasa.com/clientes/images/n-images/post-venta.jpg'); background-size: contain; background-position: center bottom;" data-bottom-top="background-position:0px 550px;" data-top-bottom="background-position:0px -500px;">
+<section id="page-title" class="page-title-parallax page-title-dark" style="padding: 250px 0; background-color: #ff5733; background-image: url('//cdn.concasa.com/concasa.com/images/n-images/post-venta.jpg'); background-size: contain; background-position: center bottom;" data-bottom-top="background-position:0px 550px;" data-top-bottom="background-position:0px -500px;">
 	<!--<div id="homepageint"></div>-->
 	<div class="container clearfix">
 		<h1>POST-VENTA</h1>
